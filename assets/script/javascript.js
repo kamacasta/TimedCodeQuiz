5bb66824f76ee88737970415613c2ca3a42c23eb
kama casta <kamacasta983@gmail.com>
2021-09-22 15:14:31 -0700

    added styles to button, changed class names, and added universal variables to JS

---
+++ assets/script/javascript.js
@@ -0,0 +1,10 @@
+// Universal variables
+const timer = document.querySelector('.timer');
+const gameOver = document.querySelector('.gameOver');
+const initTest = document.querySelector('#initiateTest');
+const quiz = document.querySelector('.quiz');
+const questions = document.querySelector(".quizQuestions");
+const answers = document.querySelector('.answers');
+const results = document.querySelector('.results');
+const correct = document.querySelector('.correct')
+const incorrect= document.querySelector('.incorrect')
