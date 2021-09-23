579b9f8d6d0debacfa34d045055efd1d68df4d04
kama casta <kamacasta983@gmail.com>
2021-09-22 22:52:39 -0700

    logic added to currentQuestions and Start quiz having some issues

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,6 +1,7 @@
 // Universal variables
 const timer = document.querySelector('.timer');
 const gameOver = document.querySelector('.gameOver');
+const introduction = document.querySelector('.introduction')
 const initTest = document.querySelector('#initiateTest');
 const quiz = document.querySelector('.quiz');
 const questions = document.querySelector(".quizQuestions");
@@ -11,7 +12,50 @@ const incorrect = document.querySelector('.incorrect')
 const initials = document.querySelector('initialsInput')
 const submit = document.querySelector('#submitScore')
 
-// Event Listners
-initTest.addEventListener('click', start);
+// Buttons given Event Listners
+initTest.addEventListener('click', startQuiz);
 submit.addEventListener('click', scoreSubmit)
 
+// let variable created for timer
+let timeRemaining = 75;
+// variables created for use in logic
+let questionOn;
+let score;
+
+// function for the quiz to be initated
+// Calls timer function
+function startQuiz () {
+    // styles given by setAttribute
+    introduction.setAttribute('style', 'display: none');
+    quiz.setAttribute('style', 'display: block');
+    // timer will display the timeRemaining
+    timer.textContent= ':' + timeRemaining;
+    time();
+    questionOn = 0
+    currentQuestionOn(question[questionOn++]);
+};
+// function displays the current question the tester is on.
+function currentQuestionOn (question) {
+    // This will present the questions
+    questions.textContent = question.question
+    // Passing the arrow function into a button that will be created
+    question.answers.array.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerHTML = answer.text;
+    button.classList.add('btn');
+    // if statement checks if the input was correct or incorrect
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    answerBtn.appendChild(button);
+    button.addEventListener('click', answersResults)
+    });
+
+
+}
+// Function for timer to countdown 
+function time() {
+    let timeInterval = setInterval(function () {
+    
+    })
+}
