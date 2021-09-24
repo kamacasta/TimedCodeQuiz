437181eb0181906a270aff14071bfe03257f6887
kama casta <kamacasta983@gmail.com>
2021-09-24 16:24:17 -0700

    added logic to functions as well as created new functions

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,6 +1,6 @@
 // Universal variables
 const timer = document.querySelector('.timer');
-const gameOver = document.querySelector('.gameOver');
+const gameOver = document.querySelector('#gameOver');
 const introduction = document.querySelector('.introduction')
 const initTest = document.querySelector('#initiateTest');
 const quiz = document.querySelector('.quiz');
@@ -11,6 +11,7 @@ const correct = document.querySelector('.correct')
 const incorrect = document.querySelector('.incorrect')
 const initials = document.querySelector('initialsInput')
 const submit = document.querySelector('#submitScore')
+
 
 // Buttons given Event Listners
 initTest.addEventListener('click', startQuiz);
@@ -50,12 +51,50 @@ function currentQuestionOn (question) {
     answerBtn.appendChild(button);
     button.addEventListener('click', answersResults)
     });
-
-
 }
+
+// depending on the results of the answer the time will decrease and will display the result to quizer
+function answersResults(event) {
+    // created these variables they are depreceted without an event
+    const choosenAnswer = event.target;
+    const correctAnswer = choosenAnswer.dataset.correct;
+    // if statement for the correct notification or incorrect notification
+    // if (!Object.is) {
+    //     Object.defineProperty()
+    // }
+    if(choosenAnswer === 'true'){
+        incorrect;
+        correct;
+    } else {
+        //and if the quizers got the question incorrect the timer is decremented
+        correct;
+        incorrect;
+        // 10 second decrement
+        timeRemaining -= 10;
+    }
+    newQuestion();
+}
+
+function newQuestion () {
+    
+}
+
+
+
+
 // Function for timer to countdown 
 function time() {
     let timeInterval = setInterval(function () {
-    
-    })
-}
+         // using the let varible created gloabally timeRemaining
+        if(timeRemaining >= 1) {
+            timeRemaining --;
+            timer.textContent = 'Time: ' + timeRemaining;
+            // if the quizer runs out of time
+        }else {
+            gameOver.getElementedById = 'You ran out of time!';
+            clearInterval(timeInterval);
+            gameOver();
+        }
+            
+        }
+        )}
