dfb2c369047451155534cfac8acdc536718940b9
kama casta <kamacasta983@gmail.com>
2021-09-25 03:55:24 -0700

    added logic to the universal variables created in scoreboard.js

--- assets/script/scoreboard.js
+++ assets/script/scoreboard.js
@@ -1,6 +1,22 @@
 // universal variables
-const scoreboard = document.querySelector('.scoreboard');
+const topQuizScores = document.querySelector('.topQuizScores');
 const removeScores = document.querySelector('.removeScores');
 const returnIndex = document.querySelector('.return');
 const deleteBtn = document.querySelector('#delete');
 const initalsScore= document.querySelector('initials');
+
+// Event listner(s) listening for click to return to index.html
+returnIndex.addEventListener('click', index => {
+    // window.location will change send the quizer from scoreboard.html to index.html
+    window.location.replace('./index.html');
+});
+function scoreboard() {
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode("- " + initalsScore + " - " + topQuizScores))
+    list.appendChild(p);
+}
+
+deleteBtn.addEventListener('click', index => {
+    topQuizScores.innerHTML = "";
+    localStorage.clear();
+})
