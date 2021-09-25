424217694f379ccd1f5348ba1c4024f0f4603882
kama casta <kamacasta983@gmail.com>
2021-09-25 03:35:55 -0700

    added universal variables to scoreboard.js needed to change names of classes due to bulma

--- assets/script/scoreboard.js
+++ assets/script/scoreboard.js
@@ -1,2 +1,6 @@
 // universal variables
-const
+const scoreboard = document.querySelector('.scoreboard');
+const removeScores = document.querySelector('.removeScores');
+const returnIndex = document.querySelector('.return');
+const deleteBtn = document.querySelector('#delete');
+const initalsScore= document.querySelector('initials');
